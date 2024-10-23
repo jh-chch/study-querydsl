686ab2cca91adc8845a950d637eca706bb65af32
jhchoi <jhchoig9@gamil.com>
2024-10-23 13:42:44 +0900

    들여쓰기 수정

--- src/test/java/com/spring/querydsl/QuerydslProjectionTests.java
+++ src/test/java/com/spring/querydsl/QuerydslProjectionTests.java
@@ -26,115 +26,116 @@ import jakarta.persistence.TypedQuery;
 @Transactional
 public class QuerydslProjectionTests {
 
-    @Autowired
-    EntityManager em;
+	@Autowired
+	EntityManager em;
 
-    JPAQueryFactory queryFactory;
+	JPAQueryFactory queryFactory;
 
-    @BeforeEach
-    void testEntity() {
-        queryFactory = new JPAQueryFactory(em);
+	@BeforeEach
+	void testEntity() {
+		queryFactory = new JPAQueryFactory(em);
 
-        Team teamA = new Team("teamA");
-        Team teamB = new Team("teamB");
-        em.persist(teamA);
-        em.persist(teamB);
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
 
-        Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 20, teamA);
-        Member member3 = new Member("member3", 30, teamB);
-        Member member4 = new Member("member4", 40, teamB);
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(member3);
-        em.persist(member4);
-    }
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+	}
 
-    @DisplayName("프로젝션 대상 하나 조회, select 대상 지정")
-    @Test
-    void simpleProjection() {
-        List<Member> result = queryFactory
-                .select(member)
-                .from(member)
-                .fetch();
+	@DisplayName("프로젝션 대상 하나 조회, select 대상 지정")
+	@Test
+	void simpleProjection() {
+		List<Member> result = queryFactory
+				.select(member)
+				.from(member)
+				.fetch();
 
-        List<String> result2 = queryFactory
-                .select(member.username)
-                .from(member)
-                .fetch();
-    }
+		List<String> result2 = queryFactory
+				.select(member.username)
+				.from(member)
+				.fetch();
+	}
 
-    @DisplayName("프로젝션 대상이 둘 이상 조회, 튜플")
-    @Test
-    void tupleProjection() {
-        List<Tuple> result = queryFactory
-                .select(member.username, member.age)
-                .from(member)
-                .fetch();
+	@DisplayName("프로젝션 대상이 둘 이상 조회, 튜플")
+	@Test
+	void tupleProjection() {
+		List<Tuple> result = queryFactory
+				.select(member.username, member.age)
+				.from(member)
+				.fetch();
 
-        for (Tuple tuple : result) {
-            System.out.println(tuple.get(member.username));
-            System.out.println(tuple.get(member.age));
-        }
-    }
+		for (Tuple tuple : result) {
+			System.out.println(tuple.get(member.username));
+			System.out.println(tuple.get(member.age));
+		}
+	}
 
-    @DisplayName("프로젝션 대상이 둘 이상 조회, DTO")
-    @Test
-    void dtoProjection() {
-        // 순수 JPA에서 DTO 조회, JPQL 문법
-        // 복잡하고, 생성자 방식만 지원한다.
-        TypedQuery<MemberDto> query = em.createQuery(
-                "select new com.spring.querydsl.dto.MemberDto(m.username, m.age)" +
-                        " from Member m",
-                MemberDto.class);
-        List<MemberDto> resultList = query.getResultList();
-        for (MemberDto dto : resultList) {
-            System.out.println(dto.getUsername());
-        }
+	@DisplayName("프로젝션 대상이 둘 이상 조회, DTO")
+	@Test
+	void dtoProjection() {
+		// 순수 JPA에서 DTO 조회, JPQL 문법
+		// 복잡하고, 생성자 방식만 지원한다.
+		TypedQuery<MemberDto> query = em.createQuery(
+				"select new com.spring.querydsl.dto.MemberDto(m.username, m.age)" +
+						" from Member m",
+				MemberDto.class);
+		List<MemberDto> resultList = query.getResultList();
+		for (MemberDto dto : resultList) {
+			System.out.println(dto.getUsername());
+		}
 
-        // Querydsl
+		// Querydsl
 
-        // 1. setter(기본 생성자 필요)
-        queryFactory
-                .select(Projections.bean(MemberDto.class,
-                        member.username,
-                        member.age))
-                .from(member)
-                .fetch();
+		// 1. setter(기본 생성자 필요)
+		queryFactory
+				.select(Projections.bean(MemberDto.class,
+						member.username,
+						member.age))
+				.from(member)
+				.fetch();
 
-        // 2. 필드 (getter, setter 필요없음)
-        queryFactory
-                .select(Projections.fields(MemberDto.class,
-                        member.username,
-                        member.age))
-                .from(member)
-                .fetch();
+		// 2. 필드 (getter, setter 필요없음)
+		queryFactory
+				.select(Projections.fields(MemberDto.class,
+						member.username,
+						member.age))
+				.from(member)
+				.fetch();
 
-        // 3. 생성자
-        queryFactory
-                .select(Projections.constructor(MemberDto.class,
-                        member.username, // .as("name") 로 alias를 줄 수 있다.
-                        member.age))
-                .from(member)
-                .fetch();
+		// 3. 생성자
+		queryFactory
+				.select(Projections.constructor(MemberDto.class,
+						member.username, // .as("name") 로 alias를 줄 수 있다.
+						member.age))
+				.from(member)
+				.fetch();
 
-        // 4. ExpressionUtils 사용
-        // ExpressionUtils.as(JPAExpressions.select(...), null)
-    }
+		// 4. ExpressionUtils 사용
+		// ExpressionUtils.as(JPAExpressions.select(...), null)
+	}
 
-    @DisplayName("@QueryProjection 사용")
-    @Test
-    void queryProjection() {
-        // dto 생성자에 @QueryProjection 어노테이션을 달아주고, complie하면 dto Q파일이 생성된다.
-        List<MemberDto> result = queryFactory
-                .select(new QMemberDto(member.username, member.age)) // 생성자 방식은 런타임 오류 발생하지만 생성자 필드를 추가해도 컴파일시 오류를 잡을 수
-                                                                     // 있다. 다만, 1. QMemberDto 파일을 추가 생성해야하는 단점이 있다. 2.
-                                                                     // DTO가 Querydsl에 의존적이게 된다.
-                .from(member)
-                .fetch();
-        for (MemberDto dto : result) {
-            System.out.println(dto.getUsername());
-            System.out.println(dto.getAge());
-        }
-    }
+	@DisplayName("@QueryProjection 사용")
+	@Test
+	void queryProjection() {
+		// dto 생성자에 @QueryProjection 어노테이션을 달아주고, complie하면 dto Q파일이 생성된다.
+		List<MemberDto> result = queryFactory
+				.select(new QMemberDto(member.username, member.age)) // 생성자 방식은 런타임 오류 발생하지만 생성자 필드를 추가해도 컴파일시 오류를 잡을 수
+																		// 있다. 다만, 1. QMemberDto 파일을 추가 생성해야하는 단점이 있다.
+																		// 2.
+																		// DTO가 Querydsl에 의존적이게 된다.
+				.from(member)
+				.fetch();
+		for (MemberDto dto : result) {
+			System.out.println(dto.getUsername());
+			System.out.println(dto.getAge());
+		}
+	}
 }
